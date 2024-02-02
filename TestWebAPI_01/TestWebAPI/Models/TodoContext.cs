baa96d00f2d36c071947ce763164a1a59c79fa11
Manasa Mudunuru <Manasa.Mudunuru@nttdata.com>
2024-02-02 10:09:18 +0530

    initialcommit

---
+++ TestWebAPI_01/TestWebAPI/Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace TestWebAPI.Models
+{
+    //public class TodoContext
+    
+        public class TodoContext : DbContext
+        {
+            public TodoContext(DbContextOptions<TodoContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TodoItem> TodoItems { get; set; } = null!;
+        }
+    }
+
